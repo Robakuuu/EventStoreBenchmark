7fa3a4c8ca5ba528e32dc71b6e9b78505da410dd
Daniel Robaszkiewicz <daniel.robaszkiewicz@wp.pl>
2024-04-03 17:08:39 +0200

    added video test

--- EventStoreBenchmark/Program.cs
+++ EventStoreBenchmark/Program.cs
@@ -28,6 +28,10 @@ namespace EventStoreBenchmark
         private byte[] _serializedData2097152;
         private byte[] _serializedData4194304;
         private byte[] _serializedData8388608;
+        private byte[] _sampleHDVideoFrameBW;
+        private byte[] _sampleHDVideoFrameCOLOR;
+        private byte[] _sampleFullHDVideoFrameBW;
+        private byte[] _sampleFullHDVideoFrameCOLOR;
 
         private string CreateStringWithSpecificLength(int length)
         {
@@ -52,7 +56,11 @@ namespace EventStoreBenchmark
            _serializedData2097152 = GetSerializedEventWithSpecificSize(2097152);
             _serializedData4194304 = GetSerializedEventWithSpecificSize(4194304);
             _serializedData8388608 = GetSerializedEventWithSpecificSize(8388608);
-        
+            _sampleHDVideoFrameBW = new byte[1080 * 720 ]; // 1 channel, hd frame
+            _sampleHDVideoFrameCOLOR = new byte[1080 * 720 * 3]; // 3 channel, hd frame
+            _sampleFullHDVideoFrameBW = new byte[1920 * 1080 ]; // 1 channel, full hd frame
+            _sampleFullHDVideoFrameCOLOR = new byte[1920 * 1080 * 3]; // 3 channel, full hd frame
+
         }
 
         private byte[] GetSerializedEventWithSpecificSize(int sizeInBytes)
@@ -62,37 +70,70 @@ namespace EventStoreBenchmark
             return JsonSerializer.SerializeToUtf8Bytes(ev);
         }
 
-        [Benchmark]
+       // [Benchmark]
         [InvocationCount(1)]
         public async Task Append512BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData512);
-        [Benchmark]
+       // [Benchmark]
         [InvocationCount(1)]
          public async Task Append4196BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData4196);
         
-        [Benchmark]
+       // [Benchmark]
         [InvocationCount(1)]
          public async Task Append65536BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData65536);
         
-        [Benchmark]
+       // [Benchmark]
         [InvocationCount(1)]
          public async Task Append262144BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData262144);
-         [Benchmark]
+        // [Benchmark]
          [InvocationCount(1)]
          public async Task Append1048576BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData1048576);
-        [Benchmark]
+       // [Benchmark]
         [InvocationCount(1)]
         public async Task Append2097152BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData2097152);
 
-        [Benchmark]
+        //[Benchmark]
         [InvocationCount(1)]
         public async Task Append4194304BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData4194304);
 
-        [Benchmark]
+        //[Benchmark]
         [InvocationCount(1)]
         public async Task Append8388608BytesEvent() => await this.AppendSerializedEventToExistingStream(_serializedData8388608);
 
+        [Benchmark]
+        [InvocationCount(1)]
+        public async Task AppendHDBWVideoFrameEvent() => await this.AppendSerializedEventToExistingStream(typeof(VideoFrameSent), _sampleHDVideoFrameBW);
+
+        [Benchmark]
+        [InvocationCount(1)]
+        public async Task AppendHDColorVideoFrameEvent() => await this.AppendSerializedEventToExistingStream(typeof(VideoFrameSent), _sampleHDVideoFrameCOLOR);
 
 
+        [Benchmark]
+        [InvocationCount(1)]
+        public async Task AppendFullHDBWVideoFrameEvent() => await this.AppendSerializedEventToExistingStream(typeof(VideoFrameSent), _sampleFullHDVideoFrameBW);
+
+
+        [Benchmark]
+        [InvocationCount(1)]
+        public async Task AppendFullHDColorVideoFrameEvent() => await this.AppendSerializedEventToExistingStream(typeof(VideoFrameSent), _sampleFullHDVideoFrameCOLOR);
+
+
+
+
+        public async Task AppendSerializedEventToExistingStream(Type eventType,byte[] data)
+        {
+            await _client.AppendToStreamAsync(
+                $"Movie-{_threadId}",
+                StreamState.Any, new[]
+                {
+                    new EventData(
+                        Uuid.NewUuid(),
+                        nameof(eventType),
+                        data, null,"application/octet-stream")
+                }
+
+            );
+        }
         public async Task AppendSerializedEventToExistingStream(byte[] data)
         {
             await _client.AppendToStreamAsync(
---
+++ EventStoreBenchmark/VideoFrameSent.cs
@@ -0,0 +1,7 @@
+﻿namespace EventStoreBenchmark;
+
+public record VideoFrameSent
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required byte[] Data { get; set; }
+}
